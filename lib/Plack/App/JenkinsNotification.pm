31c20070f2d6c46490b72468f29898014269b675
c9s <cornelius.howl@gmail.com>
2012-05-26 11:25:29 +0800

    First commit

---
+++ lib/Plack/App/JenkinsNotification.pm
@@ -0,0 +1,32 @@
+package Plack::App::JenkinsNotification;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::JenkinsNotification -
+
+=head1 SYNOPSIS
+
+  use Plack::App::JenkinsNotification;
+
+=head1 DESCRIPTION
+
+Plack::App::JenkinsNotification is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
